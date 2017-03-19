4c9247cd810caf7c4e7d125f9e24e844dc0bbb45
Clancy Childs <clancychilds@users.noreply.github.com>
2017-03-19 00:28:05 +0000

    add dropdowns

--- src/containers/MenuForm.js
+++ src/containers/MenuForm.js
@@ -4,13 +4,32 @@ import React, {
 } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-
+import Select from 'react-select';
+// Be sure to include styles at some point, probably during your bootstrapping
+import 'react-select/dist/react-select.css';
 import {} from '../actions/';
+
+const weekOptions = [
+    { value: '1', label: 'Week 1' },
+    { value: '2', label: 'Week 2' },
+    { value: '3', label: 'Week 3'}
+];
+
+const dayOptions = [
+    { value: 'mo', label: 'Monday' },
+    { value: 'tu', label: 'Tuesday' },
+    { value: 'we', label: 'Wednesday' },
+    { value: 'th', label: 'Thursday' },
+    { value: 'fr', label: 'Friday' }
+];
 
 class MenuForm extends Component {
   render() {
     const { actions } = this.props;
-    return <div>TEST</div>;
+    return <div>
+    <Select options={weekOptions} placeholder="Select the week" />
+    <Select options={dayOptions} placeholder="Select the day" />
+    </div>;
   }
 }
 
